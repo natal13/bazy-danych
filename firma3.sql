1514754762c9f3381db00db0755f010599f72191
Natalia G <61628135+natal13@users.noreply.github.com>
2020-06-03 13:32:41 +0200

    Create firma3.sql

---
+++ firma3.sql
@@ -0,0 +1,24 @@
+pg_dump firma -f firmaD.sql
+
+--
+USE master ;  
+GO  
+DROP DATABASE firmaD ;  
+GO 
+
+--
+RESTORE firmaD
+FROM DISK = 'E:\DBBackups\mydb.bak'
+
+--
+BACKUP DATABASE AdventureWorks2017
+TO DISK = 'C:\Program Files\SQL\pakiet\bazy\AdventureW17' WITH FORMAT;
+
+--
+
+CREATE VIEW view_log AS SELECT employee.loginID as login,
+  department.name AS department
+  FROM AdventureWorks2017.HumanResources.Employee AS employee,
+  AdventureWorks2017.HumanResources.Department AS department,
+  AdventureWorks2017.HumanResources.EmployeeDepartmentHistory AS employeeDepartHist
+  WHERE DepHistory.BusinessEntityID = Employee.BusinessEntityID AND DepHistory.DepartmentID = department.departmentID
