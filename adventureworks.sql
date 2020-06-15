7b2fe06dd76a7a540cdb229d2e20aa991dbac7ce
Natalia G <61628135+natal13@users.noreply.github.com>
2020-06-15 23:59:52 +0200

    Update adventureworks.sql

--- adventureworks.sql
+++ adventureworks.sql
@@ -1,5 +1,23 @@
 
+--zad 1
 
+Create PROCEDURE cFibonacci(@licz INT)
+AS
+BEGIN
+	DECLARE @numbers table(number int)
+	DECLARE @n1 INT = 0,@n2 INT =1,@i INT=0,@t INT
+	Insert Into @numbers Values(@n1),(@n2)
+  
+	WHILE (@i<@licz-2)
+	BEGIN 			
+		Insert Into @numbers Values(@n2+@n1)
+		set @t = @n2
+		Set @n2 = @n2 + @n1
+		Set @n1 = @t
+		Set @i += 1
+	END	
+	Select * from @numbers
+END
 
 --zad 2 
 CREATE TRIGGER trigger_dml ON Person.Person FOR UPDATE AS
